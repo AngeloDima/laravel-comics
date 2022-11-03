4e79a2409d54a9599cacba6b674e81048a08fc93
Angelo <Angelodimauro91@gmail.com>
2022-11-03 11:55:03 +0100

    +scss file +partials file layout

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,35 @@
-<h1>header</h1>
+<div class="header1">
+    <h1>asd</h1>
+</div>
+<div class="header2"></div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <img src="{{ asset('images/dc-logo.png') }}" alt="DC" /> -->
