1b3371eff0523cb52fb377b6528560f667310e30
Angelo <Angelodimauro91@gmail.com>
2022-11-03 18:26:26 +0100

    footer grande

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,54 @@
-<div class="containerFooter1"></div>
+<div class="containerFooter1">
+    <img src="{{ asset('images/dc-logo-bg.png') }}" alt="DC" /> 
+
+    <div class="SX">
+        <div class="cont1">
+            <h3>DC COMICS</h3>
+        <a href="">ciao</a>
+        <a href="">ciao</a>
+        <a href="">ciao</a>
+        <a href="">ciao</a>
+        <a href="">ciao</a>
+        <a href="">ciao</a>
+        <a href="">ciao</a>
+        </div>
+
+        <div class="cont2">
+        <h3>DC</h3>
+        <a href="">ciao</a>
+        <a href="">ciao</a>
+        <a href="">ciao</a>
+        <a href="">ciao</a>
+        <a href="">ciao</a>
+        <a href="">ciao</a>
+        <a href="">ciao</a>
+        <a href="">ciao</a>
+        <a href="">ciao</a>
+        <a href="">ciao</a>
+        <a href="">ciao</a>
+        </div>
+
+        <div class="cont3">
+        <h3>SITES</h3>
+        <a href="">ciao</a>
+        <a href="">ciao</a>
+        <a href="">ciao</a>
+        <a href="">ciao</a>
+        <a href="">ciao</a>
+        </div>
+
+
+        <div class="cont4">
+        <h3>SHOP</h3>
+        <a href="">ciao</a>
+        <a href="">ciao</a>
+        <a href="">ciao</a>
+        <a href="">ciao</a>
+        <a href="">ciao</a>
+        </div>
+    </div>
+
+</div>
 
 
 <div class="containerFooter2">
