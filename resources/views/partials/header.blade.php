9f9f5d37cca5bd72919f5b4be9c279efdee231d7
Angelo <Angelodimauro91@gmail.com>
2022-11-03 12:30:31 +0100

    header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,28 @@
 <div class="header1">
-    
+    <div class="containerP">
+        <p>DC POWER VISA</p>
+        <p>ADDITIONAL DC SITE</p>
+    </div>
 </div>
-<div class="header2"></div>
+
+
+
+<div class="header2">
+    <div class="container-nav">
+    <img src="{{ asset('images/dc-logo.png') }}" alt="DC" /> 
+    <a href="">CHARACTERS</a>
+    <a href="">COMICS</a>
+    <a href="">MOVIES</a>
+    <a href="">TV</a>
+    <a href="">GAMES</a>
+    <a href="">COLLECTIBLES</a>
+    <a href="">VIDEOS</a>
+    <a href="">FANS</a>
+    <a href="">NEWS</a>
+    <a href="">SHOP</a>
+    <input type="search" name="" id="">
+    </div>
+</div>
 
 
 
@@ -32,4 +53,3 @@
 
 
 
-<!-- <img src="{{ asset('images/dc-logo.png') }}" alt="DC" /> -->
